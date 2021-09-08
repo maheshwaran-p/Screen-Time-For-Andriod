feeb13b31e967a882e863e3a00550eab7fd497ac
maheshwaran-p <mpmahesh1022@gmail.com>
2021-09-08 11:37:33 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:app_usage/app_usage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  List<AppUsageInfo> _infos = [];
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  void getUsageStats() async {
+    try {
+      DateTime endDate = new DateTime.now();
+      DateTime startDate = endDate.subtract(Duration(hours: 2));
+      List<AppUsageInfo> infoList =
+          await AppUsage.getAppUsage(startDate, endDate);
+      setState(() {
+        _infos = infoList;
+      });
+
+      for (var info in infoList) {
+        print(info.toString());
+      }
+    } on AppUsageException catch (exception) {
+      print(exception);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My Screen Time'),
+          backgroundColor: Colors.green,
+        ),
+        body: ListView.builder(
+            itemCount: _infos.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                  title: Text(_infos[index].appName),
+                  trailing: Text(_infos[index].usage.toString()));
+            }),
+        floatingActionButton: FloatingActionButton(
+            onPressed: getUsageStats, child: Icon(Icons.file_download)),
+      ),
+    );
+  }
+}
