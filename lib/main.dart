fdfd29ff01ca61b7e3b56ef51a948241feb21d6b
maheshwaran-p <mpmahesh1022@gmail.com>
2022-03-03 23:43:12 +0530

    screen time working

--- lib/main.dart
+++ lib/main.dart
@@ -1,84 +1,69 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'package:app_usage/app_usage.dart';
 import 'package:flutter/material.dart';
 import 'package:workmanager/workmanager.dart';
 import 'dart:io' show Platform;
-
-import 'package:teledart/teledart.dart';
-import 'package:teledart/telegram.dart';
+//import 'package:flutter_email_sender/flutter_email_sender.dart';
 
 void main() {
   runApp(MyApp());
 }
 
-const simpleTaskKey = "simpleTask";
-//const starttele = "simplePeriodicTask";
-
 const simplePeriodicTask = "simplePeriodicTask";
 
 void callbackDispatcher() {
   Workmanager().executeTask((task, inputData) async {
+    print("bye");
+    print(task);
     switch (task) {
-      case simpleTaskKey:
-        print("$simplePeriodicTask was executed2222");
-        var bOTTOKEN = '5038745862:AAFWBn8quZcLeGbgrxAo0eKvTgkS6KLz3a8';
-        print('hiii');
-        final username = (await Telegram(bOTTOKEN).getMe()).username;
-        print('bye....');
-        var teledart = TeleDart(bOTTOKEN, Event(username!));
-
-        teledart.start();
-        print('kabiiiii');
-
-        teledart.onMessage(keyword: 'start').listen((message) {
-          print(message);
-          message.reply('Stand with Hong Kong');
-        });
-        print('maheeeeee');
-
-        teledart
-            .onMessage(entityType: 'bot_command', keyword: 'check')
-            .listen((message) async {
-          print('entereeen');
-          teledart.getChat(message.chat.id);
-          await teledart.sendMessage(message.chat.id,
-              'Great News BackGround Services Started Successfully!');
-        });
-        print('okkkkk');
-        break;
-
       case simplePeriodicTask:
-        print("$simplePeriodicTask was executed");
-        var bOTTOKEN = '5038745862:AAFWBn8quZcLeGbgrxAo0eKvTgkS6KLz3a8';
-        print('hiii');
-        final username = (await Telegram(bOTTOKEN).getMe()).username;
-        print('bye....');
-        var teledart = TeleDart(bOTTOKEN, Event(username!));
+        print("task entered");
+        getUsageStats();
 
-        teledart.start();
-        print('kabiiiii');
-
-        teledart.onMessage(keyword: 'start').listen((message) {
-          // print(message);
-          message.reply('Stand with Hong Kong');
-        });
-        print('maheeeeee');
-
-        teledart
-            .onCommand('start')
-            .listen((message) => message.reply('This works too!'));
-        print('okkkkk');
         break;
     }
-
     return Future.value(true);
   });
+}
+
+void getUsageStats() async {
+  print("call entered");
+  try {
+    DateTime endDate = new DateTime.now();
+    DateTime startDate = endDate.subtract(Duration(hours: 2));
+    List<AppUsageInfo> infoList =
+        await AppUsage.getAppUsage(startDate, endDate);
+
+    for (var info in infoList) {
+      print(info.toString());
+    }
+  } on AppUsageException catch (exception) {
+    print(exception);
+  }
 }
 
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
+}
+
+enum _Platform { android, ios }
+
+class PlatformEnabledButton extends ElevatedButton {
+  final _Platform platform;
+
+  PlatformEnabledButton({
+    required this.platform,
+    required Widget child,
+    required VoidCallback onPressed,
+  }) : super(
+            child: child,
+            onPressed: (Platform.isAndroid && platform == _Platform.android ||
+                    Platform.isIOS && platform == _Platform.ios)
+                ? onPressed
+                : null);
 }
 
 class _MyAppState extends State<MyApp> {
@@ -100,6 +85,7 @@ class _MyAppState extends State<MyApp> {
               ElevatedButton(
                   child: Text("Start the Flutter background service"),
                   onPressed: () {
+                    print("background services started");
                     Workmanager().initialize(
                       callbackDispatcher,
                       isInDebugMode: true,
@@ -111,8 +97,8 @@ class _MyAppState extends State<MyApp> {
               ),
               //This task runs once.
               //Most likely this will trigger immediately
-              FlatButton(
-                //  platform: _Platform.android,
+              PlatformEnabledButton(
+                platform: _Platform.android,
                 child: Text("take Location"),
                 onPressed: () {
                   Workmanager().registerPeriodicTask("10", simplePeriodicTask,
@@ -122,23 +108,16 @@ class _MyAppState extends State<MyApp> {
                       ));
                 },
               ),
-
-              FlatButton(
-                // platform: _Platform.android,
-                child: Text("Start the Bot"),
-                onPressed: () async {
-                  Workmanager().registerOneOffTask("1", "simpleTask",
-                      constraints: Constraints(
-                          networkType: NetworkType.connected,
-                          requiresBatteryNotLow: true,
-                          requiresCharging: true,
-                          requiresDeviceIdle: true,
-                          requiresStorageNotLow: true));
+              PlatformEnabledButton(
+                platform: _Platform.android,
+                child: Text("take screen time"),
+                onPressed: () {
+                  getUsageStats();
                 },
               ),
 
-              FlatButton(
-                // platform: _Platform.android,
+              PlatformEnabledButton(
+                platform: _Platform.android,
                 child: Text("Cancel All"),
                 onPressed: () async {
                   await Workmanager().cancelAll();
