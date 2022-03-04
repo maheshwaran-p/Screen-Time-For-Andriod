f8268cded25ca107df9203e8c481dbddd42d285b
maheshwaran-p <mpmahesh1022@gmail.com>
2022-03-04 18:36:38 +0530

    again working

--- lib/main.dart
+++ lib/main.dart
@@ -31,21 +31,6 @@ void callbackDispatcher() async {
   });
 }
 
-List<AppUsageInfo> infoList = [];
-void getusagepermission() async {
-  try {
-    DateTime endDate = new DateTime.now();
-    DateTime startDate = endDate.subtract(Duration(hours: 2));
-    infoList = await AppUsage.getAppUsage(startDate, endDate);
-
-    for (var info in infoList) {
-      print(info.toString());
-    }
-  } on AppUsageException catch (exception) {
-    print(exception);
-  }
-}
-
 void getUsageStats() async {
   print("call entered");
   try {
@@ -57,42 +42,38 @@ void getUsageStats() async {
     for (var info in infoList) {
       print(info.toString());
     }
-    sendmail();
+    String username = 'mahe.1817130@gct.ac.in';
+    String password = 'gct@1234';
+
+    final smtpServer = gmail(username, password);
+
+    final message = Message()
+      ..from = Address(username, 'ScreenTime')
+      ..recipients.add('mpmahesh1022@gmail.com')
+      ..subject = 'ScreenTime:\n ${infoList[16]}\n${infoList[3]} '
+      ..text = 'This is the plain text.\nThis is line 2 of the text part.'
+      ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>";
+
+    try {
+      final sendReport = await send(message, smtpServer);
+      print('Message sent: ' + sendReport.toString());
+    } on MailerException catch (e) {
+      print('Message not sent.');
+      for (var p in e.problems) {
+        print('Problem: ${p.code}: ${p.msg}');
+      }
+    }
+
+    var connection = PersistentConnection(smtpServer);
+
+    // Send the first message
+    await connection.send(message);
+
+    // close the connection
+    await connection.close();
   } on AppUsageException catch (exception) {
     print(exception);
   }
-}
-
-void sendmail() async {
-  print('email function called');
-  String username = 'mahe.1817130@gct.ac.in';
-  String password = 'gct@1234';
-
-  final smtpServer = gmail(username, password);
-
-  final message = Message()
-    ..from = Address(username, 'Screen Time')
-    ..recipients.add('kgaah1213@gmail.com')
-    ..subject = 'Test Dart Mailer library :: 😀 ::'
-    ..text = 'This is the plain text.\nThis is line 2 of the text part.'
-    ..html = "<h1>Test</h1>\n<p>Hey! Here's some HTML content</p>";
-
-  try {
-    print('try called');
-    final sendReport = await send(message, smtpServer);
-    print('Message sent: ' + sendReport.toString());
-  } on MailerException catch (e) {
-    print('Message not sent.');
-    for (var p in e.problems) {
-      print('Problem: ${p.code}: ${p.msg}');
-    }
-  }
-
-  var connection = PersistentConnection(smtpServer);
-
-  await connection.send(message);
-
-  await connection.close();
 }
 
 class MyApp extends StatefulWidget {
@@ -163,7 +144,7 @@ class _MyAppState extends State<MyApp> {
                 platform: _Platform.android,
                 child: Text("take screen time"),
                 onPressed: () {
-                  getusagepermission();
+                  getUsageStats();
                 },
               ),
 
